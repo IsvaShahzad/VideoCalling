a643329d3bfae0689723bd23f5229e6b67f8bc86
Isva shahzad <isvashaz@gmail.com>
2025-05-26 15:45:32 +0500

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-// main.dart
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:video_app/screens/sign_up.dart';
+import 'package:video_app/screens/sign_up.dart'; // Make sure this file and class exist
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,29 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My App',
-      home: const HomeScreen(), // Rename MyHomePage to HomeScreen or match the class name you actually defined
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to your LoginScreen
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) =>  EmailSignUpScreen()),
-            );
-          },
-          child: const Text("Go to Login"),
-        ),
-      ),
+      home:  EmailSignUpScreen(),
     );
   }
 }
